d2242f8f24a16d34a3e57ec7a0951b55696b91bb
crhodes <connorrhodes1@yahoo.com>
2018-01-19 02:23:29 -0800

    final touches

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veg-E Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Heart Attack Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', true);
