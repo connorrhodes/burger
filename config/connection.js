1c24b9e67ac52e2736ec6bfdce3dd322cd3b7766
crhodes <connorrhodes1@yahoo.com>
2018-01-19 15:09:41 -0800

    Changed . to =

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,9 @@ var mysql = require('mysql');
 // Create the MySQL connection object
 var connection;
 
-
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 	// DB is local on localhost
 	connection = mysql.createConnection({
 		port: 3306,
@@ -14,7 +16,7 @@ var connection;
 		database: 'burgers_db'
 	});
 
-
+}
 // Make the connection to MySQL
 connection.connect(function(err) {
   if (err) {
